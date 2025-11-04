f31679dea59a8704dab6523053235c8e7c10b69b
abdullah201240 <abdullahalsakib7075@gmail.com>
2025-11-04 12:41:03 +0600

    fix(next): add new allowed image domains and remote patterns
    - Add '192.168.21.241' and '192.168.21.241:3000' to image domains
    - Include new remote pattern for 'http://192.168.21.241:3000/uploads/**' in next config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'localhost:3000'],
+    domains: ['localhost', 'localhost:3000', '192.168.21.241', '192.168.21.241:3000'],
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
+        port: '3000',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '192.168.21.241',
         port: '3000',
         pathname: '/uploads/**',
       },
